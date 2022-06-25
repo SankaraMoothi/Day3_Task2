aed4c8f91b80d863e023ec3c4329709a66b2bf6c
Sankara_Moorthi <107635975+SankaraMoothi@users.noreply.github.com>
2022-06-25 14:11:47 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,99 @@
+var RESUME=
+{
+  "basics": {
+    "name": "Sankara_Moorthi.b",
+    "label": "Full_Stuck_developer",
+    "image": "sankar.jpg",
+    "email": "sankarmoorthi99@gmail.com",
+    "phone": "6379883546",
+    "url": "https://github.com/SankaraMoothi",
+    "summary": "A Learner How Eager To Learn New Thing And End Of Day Add An Little Bit Of Extra Knowledge Compare To Yesterday Really The Size Not A Matter But The Consistency",
+    "location": {
+      "address": "palani",
+      "postalCode": "624613",
+      "city": "palani",
+      "countryCode": "Indian",
+      "region": "Asian"
+    },
+    "profiles": [{
+      "network": "instagram",
+      "username": "mass_sankar_29",
+      "url": "https://instagram.com/mass_sankar_29"
+    }]
+  },
+  "work": [{
+    "name": "Guvi",
+    "position": "Student",
+    "url": "https://guvi.in",
+    "startDate": "2022-06-19",
+    "endDate": "Course end date is my last day",
+    "summary": "It is an online platform to learn coading"
+  }],
+ 
+  "education": [{
+    "institution": "Anna University",
+    "url": "https://SSM institution.com/",
+    "area": "Mechanical Engineering",
+    "studyType": "Bachelor",
+    "startYear": "2017",
+    "endYear": "2021",
+    "score": "7.7"
+  }],
+  "awards": [{
+    "title": "Class ref",
+    "date": "2020-11-01",
+    "awarder": "Class Teacher",
+    "summary": "Because I am alway Out standing student of class for late coming so she teach an leason in awarding ways"
+  }],
+  "certificates": [{
+    "name": "javaCertificate",
+    "date": "2022-06-15",
+    "issuer": "Arun ceo of Guvi",
+    "url": "https://certificate.com"
+  }],
+  "publications": [{
+    "name": "http1.1 vs http 2",
+    "publisher": "Mass_sankar",
+    "releaseDate": "last week",
+    "url": "https://medium_publication.com",
+    "summary": "I just write about different between Http1.1 and Http2"
+  }],
+  "skills": [{
+    "name": "Fast Leaner",
+    "level": "Master",
+    "keywords": [
+      "HTML",
+      "CSS",
+      "JavaScript"
+    ]
+  }],
+  "languages": [{
+    "language": "English_Tamil_Malayalam",
+    "fluency": "Native speaker"
+  }],
+  "interests": [{
+    "name": "Wildlife,music",
+    "keywords": [
+      "Ferrets",
+      "Unicorns",
+      "ARR",
+      "u1"
+    ]
+  }],
+  "projects": [{
+    "name": "TIG-Welding",
+    "description": "TIG-Welding using parent material as an filler material",
+    "highlights": [
+      "Won It Passes Anna University Viva Stage Without Question"
+    ],
+    "keywords": [
+      "Welding"
+    ],
+    "startDate": "2020-12-15",
+    "endDate": "2021-01-01",
+    "url": "https://sankar_welding_project.com/",
+    "roles": [
+      "Team Lead"
+    ],
+  }]
+}
